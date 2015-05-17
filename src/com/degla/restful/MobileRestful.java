e7e3a229cee36b3c47e845095cfae6b80ae1d505
Mohamed Ibrahim <msami.fawzi@gmail.com>
2015-05-17 15:51:41 +0300

    Route Files to different Employees

---
+++ src/com/degla/restful/MobileRestful.java
@@ -0,0 +1,41 @@
+package com.degla.restful;
+import com.degla.system.SpringSystemBridge;
+import com.degla.system.SystemService;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Path;
+
+/**
+ * Created by snouto on 17/05/15.
+ */
+// The Java class will be hosted at the URI path "/helloworld"
+@Path("/")
+public class MobileRestful {
+
+    private SystemService systemService;
+
+
+
+    public MobileRestful()
+    {
+        try
+        {
+            systemService = SpringSystemBridge.services();
+
+        }catch(Exception s)
+        {
+            s.printStackTrace();
+        }
+    }
+
+    //Login method
+    @GET
+    @Produces("text/plain")
+    public String helloWorld()
+    {
+        return "Hello World";
+    }
+
+}
